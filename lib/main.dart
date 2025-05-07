c286efb7f596aa16a60e5953c58960c17bfe62bb
lindgh <lindaghun108@gmail.com>
2025-05-06 18:08:35 -0700

    adding firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import '../pages/nav_bar.dart';
 
-void main() {
+//must include bottom two for firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
