3520c189276cf616ecc251cad58f763ca99ea2ca
lindgh <lindaghun108@gmail.com>
2025-05-08 14:07:31 -0700

    implemented create profile page without database integration

--- lib/pages/create_profile.dart
+++ lib/pages/create_profile.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
+import 'text_field.dart';
+import 'nav_bar.dart';
 
 class CreateProfilePage extends StatelessWidget {
   const CreateProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // THIS IS FOR THIS PAGE'S CONTENTS.
-    // return Container(
-    //     color: Colors.pink,
-    //     child: const Center(
-    //         child: Text(
-    //           "Create your profile! :3",
-    //         )
-    //     )
-    // );
     return Scaffold(
-      appBar: AppBar(title: const Text('Create your profile')),
-      backgroundColor: Colors.pink,
-      // body: Center(
-      //   child: ElevatedButton(
-      //       onPressed: () {
-      //         Navigator.pop(context);
-      //       },
-      //       child: const Text('Go back to profile page.'),
-      //   ),
-      //)
+      appBar: AppBar(
+          title: const Text('Create your profile'),
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+          leadingWidth: 100,
+      ),
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          //name
+          MyTextField(text: 'Name'),
+
+          //major
+          MyTextField(text: 'Major'),
+
+          //school
+          MyTextField(text: 'College/University'),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 100),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.indigoAccent,
+                  surfaceTintColor: Colors.indigoAccent,
+                  textStyle: TextStyle(
+                    fontSize: 18,
+                  ),
+              ),
+              onPressed: () { //When create prof button pushed: just goes to nav rn, but should store user data
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const NavBar()),
+                );
+              },
+              child: const Text('Continue'),
+            ),
+          ),
+        ],
+      ),
     ); //scaffold
   }
 }
