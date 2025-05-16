a84d4ff0a31e341fa87aed304f6b40dab01b90f1
gmapa002 <gmapa002@ucr.edu>
2025-05-16 11:17:37 -0700

    Added documentation to student card header file

--- lib/models/student_card_model.dart
+++ lib/models/student_card_model.dart
@@ -1,12 +1,61 @@
 class StudentCard {
+  /*
+    name
+    -Study Focus
+    -Bio
+   */
   final String name;
+
+  /*
+    school
+    -School
+    -Bio
+   */
   final String school;
+
+  /*
+    majorTitle
+    -Study Focus
+   */
   final String majorTitle;
+
+  /*
+    headerImagePath
+    -TOP of CARD Image (must update pubspec.yaml to add more images)
+        - images can also be used with an internet link
+   */
   final String headerImagePath;
+
+  /*
+    profileImagePath
+    - BIO pfp (must update pubspec.yaml to add more images)
+        - images can also be used with an internet link
+   */
   final String profileImagePath;
+
+  /*
+    schoolImagePath
+    -School Profile Image (must update pubspec.yaml to add more images)
+        - images can also be used with an internet link
+   */
   final String schoolImagePath;
+
+  /*
+    courses
+    - 'pills' on the study focus
+  */
   final List<String> courses;
+
+  /*
+    studyFocusText
+    - appears on study focus
+   */
   final String studyFocusText;
+
+  /*
+    bio
+    - bio text
+   */
   final String bio;
 
   StudentCard({
