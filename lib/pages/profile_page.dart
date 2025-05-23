0c49a9a71e6430dab247d932247c74e0afbe76e7
Baipon Phimmala <129914502+bphimmala@users.noreply.github.com>
2025-05-23 13:26:38 -0700

    Added logout functionality; fixed routing functionality

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/auth/authentication.dart';
 import 'create_profile.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -6,31 +7,16 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // THIS IS FOR THIS PAGE'S CONTENTS.
-
-    // return Container(
-    //     color: Colors.green,
-    //     child: const Center(
-    //         child: Text(
-    //           "Profile",
-    //         )
-    //     )
-    // );
-
     return Scaffold(
       appBar: AppBar(title: const Text('Profile Page')),
         backgroundColor: Colors.green,
         body: Center(
-          child: const Text('Profile Page'),
-          // ElevatedButton(
-          //   onPressed: () {
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute(builder: (context) => const CreateProfilePage()),
-          //     );
-          //   },
-          //   child: const Text('Create your profile'),
-          // ),
+          child: ElevatedButton(
+            onPressed: () {
+              signOut(context);
+            },
+            child: const Text('Logout'),
+          ),
         ),
 
     ); //scaffold
