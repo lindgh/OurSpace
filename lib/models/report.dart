35e769f839264dc71c22736646bfff21d32f7af5
teez-ai <songcokurt.ad@gmail.com>
2025-05-29 20:16:34 -0700

    Add report feature

---
+++ lib/models/report.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+class Report {
+  final String reporterId;
+  final String reportedUserId;
+  final String reason;
+  final DateTime timestamp;
+
+  Report({
+    required this.reporterId,
+    required this.reportedUserId,
+    required this.reason,
+    required this.timestamp,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'reporterId': reporterId,
+    'reportedUserId': reportedUserId,
+    'reason': reason,
+    'timestamp': timestamp.toIso8601String(),
+  };
+}
