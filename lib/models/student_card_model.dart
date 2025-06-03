39c1329500d4dd5f8e35b823efe0b6e14b4f74b5
bphimmala <bphimmala@gmail.com>
2025-06-02 20:30:36 -0700

    Student card UI and data updated.

--- lib/models/student_card_model.dart
+++ lib/models/student_card_model.dart
@@ -3,23 +3,15 @@ class StudentCard {
   final String UserCollege;
   final String UserMajor;
   final String UserGradYear;
-  final String headerImagePath;
   final String profileImagePath;
-  final String schoolImagePath;
-  final List<String> courses;
-  final String studyFocusText;
   final String UserBio;
 
   StudentCard({
     required this.UserName,
     required this.UserCollege,
-    required this.headerImagePath,
-    required this.profileImagePath,
-    required this.schoolImagePath,
     required this.UserMajor,
     required this.UserGradYear,
-    required this.courses,
-    required this.studyFocusText,
+    required this.profileImagePath,
     required this.UserBio,
   });
 }
